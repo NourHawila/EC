4d468044f5af9e0f0efc0a1696113706fcefd880
NourHawila <hawilanour@gmail.com>
2021-09-02 16:25:29 -0400

    2.1

---
+++ inst/CITATION.R
@@ -0,0 +1,9 @@
+citHeader("To cite EC in publications, please use:")
+
+citEntry(entry = "article",
+         author = "Nour Hawila and Arthur Berg",
+         title = "Exact-corrected confidence interval for risk difference in noninferiority binomial trials",
+         publisher = "arXiv",
+         year = "2021",
+         isbn = "978-3-319-24277-4",
+         url = "https://arxiv.org/abs/2104.04660")
