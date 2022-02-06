24a4b5d563b6cf722c81665af648d585583cf85c
NourHawila <hawilanour@gmail.com>
2022-02-06 14:54:33 -0500

    EC4.0

--- R/EC.R
+++ R/EC.R
@@ -526,10 +526,12 @@ ci_general = function(x.T, x.C, N.T, N.C, delta0, method="MN", EC, alpha=.05, to
     Delta=seq(-0.99,0.99,0.01)
     stat_EC2 = Vectorize(stat_EC,"Delta")
     zvals=unlist(stat_EC2(x.T,x.C,N.T,N.C,Delta,delta0)["Z",])
-    monot = sum(diff(zvals)<0,na.rm=TRUE)
+    ind.na = which(is.na(unlist(stat_EC2(x.T,x.C,N.T,N.C,Delta,delta0)["Z",])))
+    zvals = zvals[-ind.na]
+    monot = sum(diff(zvals)<0)
 
     if(monot==0) outp = "The statistic Z_EC is confirmed to be monotonically increasing."
-    if(monot>0) outp = monotonicity_check(Delta,zvals)
+    if(monot>0) outp = monotonicity_check(Delta[-ind.na],zvals)
   }
 
     chi2=qchisq(1-alpha,1)
